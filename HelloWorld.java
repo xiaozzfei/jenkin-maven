72defba70e34c7e75e25d73615b1031516b9ed64
xiaozzfei <57299556+xiaozzfei@users.noreply.github.com>
2022-05-15 13:29:43 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.println("Hello from Java to my update 1st");
+		System.out.println("Hello from Java to my update 2nd");
 	}
 }
